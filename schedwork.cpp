cb0d306da31e5d9ffe1a761ddf15e8b2c3333f5b
Nona Nersisyan <nnersisy@usc.edu>
2023-04-07 01:54:09 +0000

    x X X

--- schedwork.cpp
+++ schedwork.cpp
@@ -20,7 +20,42 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool scheduleHelp(
+	const AvailabilityMatrix& avail, 
+	DailySchedule& sched, 
+	const size_t dailyNeed, 
+	const size_t maxShifts, 
+	vector<int> rtn,
+	Worker_T day, 
+	Worker_T worker)
+{
+	if(day == sched.size()) return true;
 
+	if(day == avail.size()-1) 
+	if(worker == dailyNeed) return true;
+
+	if(worker == dailyNeed)
+	{
+		day++;
+		worker = 0;
+	}
+
+	for(unsigned int i = 0; i < avail[0].size(); i++)
+	{
+		if(avail[day][i] && rtn[i]<maxShifts)
+		{
+			sched[day][worker] = i;
+			rtn[i]++;
+
+			if(scheduleHelp(avail, sched, dailyNeed, maxShifts, rtn, day, worker+1))
+				return true;
+
+			sched[day][worker] = INVALID_ID;
+			rtn[i]--;
+		}
+	}
+	return false;
+}
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -36,6 +71,21 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+
+    
+    for(unsigned int i = 0; i < avail.size(); i++)
+    {
+        vector<Worker_T> sched_;
+        for(unsigned int j = 0; j < dailyNeed; j++)
+        {
+            sched_.push_back(INVALID_ID);
+        }
+        sched.push_back(sched_);
+    }
+
+		vector<int> rtn(avail[0].size(), 0);
+
+		return scheduleHelp(avail, sched, dailyNeed, maxShifts, rtn, 0, 0);
 
 
 
--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,7 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <queue>
 #endif
 
 #include "wordle.h"
@@ -13,7 +14,61 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void wordleHelp(
+    std::string& in,
+    std::queue<char> float_,
+    const std::set<std::string>& dict,
+    set<string>& rtn,
+    size_t num,
+    int dash)
+{
+    if(in.size() == 0) return;
 
+    dash = 0;
+    for(unsigned int i = 0; i < in.size(); i++)
+    {
+        if(in[i] == '-') dash++;
+    }
+
+    if(num == in.size())
+    {
+        if(dict.find(in) != dict.end()) rtn.insert(in);
+        return;
+    }
+
+    if(in[num] == '-')
+    {
+        for(unsigned int i = 0; i < float_.size(); i++)
+        {
+            if(float_.front() == '0')
+            {
+                float_.pop();
+                char i = 'a';
+                while (i <= 'z') 
+                {
+                    in[num] = i;
+                    wordleHelp(in, float_, dict, rtn, num+1, dash);
+                    i++;
+                }
+                in[num] = '-';
+                float_.push('0');
+            }
+            else
+            {
+                in[num] = float_.front();
+                float_.pop();
+                wordleHelp(in, float_, dict, rtn, num+1, dash);
+                float_.push(in[num]);
+                in[num] = '-';
+            }
+        }
+    }
+    else wordleHelp(in, float_, dict, rtn, num+1, dash);
+
+
+
+
+}
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +77,30 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    int dash = 0;
+    queue<char> float_;
+    set<string> rtn;
+    string in_ = in;
 
+    for(unsigned int i = 0; i < in.size(); i++)
+    {
+        if(in[i] == '-') dash++;
+    }
+
+    for(unsigned int i = 0; i < floating.size(); i++)
+    {
+        float_.push(floating[i]);
+    }
+
+    for(unsigned int i = 0; i < (dash-(floating.size())); i++)
+    {
+        float_.push('0');
+    }
+
+
+    wordleHelp(in_, float_, dict, rtn, 0, dash);
+
+    return rtn;
 }
 
 // Define any helper functions here
